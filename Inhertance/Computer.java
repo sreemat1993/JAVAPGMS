5c8793e1651fd202e6eebebd75a4b90d6b3fba24
SREEMAT <sreemat1993@gmail.com>
2015-07-15 21:20:09 +0530

    Create Computer.java

---
+++ Inhertance/Computer.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Computer extends Programmer {
+
+	public static void main(String[] args) {
+		Computer comp= new Computer();
+		comp.doProject();
+		comp.doCode();
+		comp.getProject();
+
+	}
+
+}
