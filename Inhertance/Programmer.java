33a9bbcb0065cd0905f0c4eb55316f3d86d33b58
SREEMAT <sreemat1993@gmail.com>
2015-07-15 21:19:10 +0530

    Create Programmer.java

---
+++ Inhertance/Programmer.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class Programmer extends Company{
+	String name="sreemat";
+	String des="Junior developer";
+public void doCode()
+{
+	System.out.println("Inside doCode");
+}
+}
