272c716a8cd0423b4c9dd4862009250c80c812c1
SREEMAT <sreemat1993@gmail.com>
2015-07-15 21:30:34 +0530

    Create EncapMain.java

---
+++ Encapsulation/EncapMain.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class EncapMain {
+
+	public static void main(String[] args) {
+		Encap en=new Encap();
+		en.display();
+		en.setAge(23);
+		//System.out.println(en.getAge());
+		en.setName("SREEMAT");
+		//System.out.println(en.getName());
+		en.display();
+
+	}
+
+}
