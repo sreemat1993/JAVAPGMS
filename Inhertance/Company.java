45909de20aae42f904e7b330cfc213dea2c95b41
SREEMAT <sreemat1993@gmail.com>
2015-07-15 21:14:18 +0530

    Create Company.java

---
+++ Inhertance/Company.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Company {
+String Com_name="ABC Limited";
+String Founder="XYZ";
+public void doProject()
+{
+	System.out.println("Inside doProject");
+}
+public void getProject()
+{
+	System.out.println("Inside getProject");
+}
+}
