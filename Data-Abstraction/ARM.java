01f97fee2b2e73c823a07fd4a7921737befe2c37
SREEMAT <sreemat1993@gmail.com>
2015-07-15 21:44:28 +0530

    Create ARM.java

---
+++ Data-Abstraction/ARM.java
@@ -0,0 +1,20 @@
+package com.example;
+
+public class ARM extends Computing {
+
+	public void howToCompute()
+	{
+		System.out.println("Inside ARM.howToCompute()");
+	}
+	public void memMang()
+	{
+		System.out.println("Inside ARM.memMang");
+	}
+	public void processMang() 
+	{
+		System.out.println("Inside ARM.processMang");
+	}
+
+	
+
+}
