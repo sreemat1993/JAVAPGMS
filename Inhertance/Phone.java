c3836e5a6563c2a8b253c657e7cf2d40dfed7e7f
SREEMAT <sreemat1993@gmail.com>
2015-07-15 21:11:06 +0530

    Create Phone.java

---
+++ Inhertance/Phone.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class Phone {
+	
+	
+	public void receiveCall(){
+		System.out.println("Calling...");
+		}
+	
+	public void dailCall()
+	{
+		System.out.println("Dailing...");
+	}
+
+}
